ca786f2185e9a091205dd1f73f22ab1fbbaf6550
Marcello Bastea-Forte <marcello@cellosoft.com>
2013-03-14 17:00:26 -0700

    initial commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+Safe start
+=================
+
+Checks if your dependencies are valid. Or throws an Error.
+
+Usage / Examples
+----------------
+
+
+```js
+var safestart = require('safestart')
+
+safestart(__dirname) // throws an Error on failure
+```
+
+Can also be used commandline:
+```
+safestart
+```
+
+License
+-------
+Open source software under the [zlib license](LICENSE).
---
+++ index.js
@@ -0,0 +1,40 @@
+var fs = require('fs')
+var path = require('path')
+var semver = require('semver')
+
+var packages = {}
+
+module.exports = function checkPath(basePath) {
+    var packageJsonPath = path.normalize(path.join(basePath, 'package.json'))
+    var packageJson = packages[packageJsonPath] 
+    if (!packageJson) {
+        if (!fs.existsSync(basePath)) {
+            return packages[packageJsonPath] = null
+        }
+        packageJson = packages[packageJsonPath] = JSON.parse(fs.readFileSync(packageJsonPath))
+    }
+
+    Object.keys(packageJson.dependencies || {}).sort().forEach(function(dependencyName) {
+        var expectedVersion = packageJson.dependencies[dependencyName]
+        var base = basePath
+        var dependecyPath  
+        var dependency
+        while (true) {
+            dependecyPath = path.join(base, 'node_modules', dependencyName)
+            dependency = checkPath(dependecyPath)
+            if (dependency) {
+                break
+            }
+            if (base == path.dirname(base)) {
+                throw new Error(packageJson.name + " dependency not found: " + dependencyName + " (expected: " + expectedVersion + ")")
+            }
+            base = path.dirname(base)
+        }
+        if (!/#/.test(expectedVersion) &&
+            expectedVersion != 'latest' &&
+                !semver.satisfies(dependency.version, expectedVersion)) {
+            throw new Error(packageJson.name + " dependency version mismatch: " + dependencyName + " from " + dependecyPath + " (expected: " + expectedVersion + ", got: " + dependency.version + ")")
+        }
+    })
+    return packageJson
+}
